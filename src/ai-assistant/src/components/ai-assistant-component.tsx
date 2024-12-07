7d44a52e65351bec8800b4ccd9f94337e77c3a8d
Xun Li <lixun910@gmail.com>
2024-12-07 01:23:28 -0700

    init

--- src/ai-assistant/src/components/ai-assistant-component.tsx
+++ src/ai-assistant/src/components/ai-assistant-component.tsx
@@ -7,7 +7,8 @@ import {
   AiAssistant,
   MessageModel,
   useAssistant,
-  histogramFunctionDefinition
+  histogramFunctionDefinition,
+  queryDuckDBFunctionDefinition
 } from 'react-ai-assist';
 import 'react-ai-assist/dist/index.css';
 
@@ -101,6 +102,10 @@ function AiAssistantComponentFactory() {
             selectedRowIndices,
             keplerGlActions.layerSetIsValid
           )
+      }),
+      queryDuckDBFunctionDefinition({
+        getValues: (datasetName: string, variableName: string): number[] =>
+          getValuesFromDataset(visState.datasets, datasetName, variableName)
       })
     ];
 
